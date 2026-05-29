377c854c94cda8dab12afb7fd8e63020fff4a500
WesSno <kofibaafik@gmail.com>
2026-05-29 13:58:21 +0000

    added Nav bar supported by JS code

---
+++ js/script.js
@@ -0,0 +1,25 @@
+const navMenu = document.querySelector(".nav-menu");
+const navBtn = document.querySelector(".nav-button");
+const hamIcon = document.querySelector(".hamburger-icon");
+const closeIcon = document.querySelector(".close-icon");
+const navItems = document.querySelectorAll(".nav-item");
+
+toggleMenu = () => {
+  if (navMenu.classList.contains("showMenu")) {
+    navMenu.classList.remove("showMenu");
+    closeIcon.style.display = "none";
+    hamIcon.style.display = "block";
+    document.body.style.overflowY = "visible";
+  } else {
+    navMenu.classList.add("showMenu");
+    closeIcon.style.display = "block";
+    hamIcon.style.display = "none";
+    document.body.style.overflowY = "hidden";
+  }
+};
+
+navBtn.addEventListener("click", toggleMenu);
+
+navItems.forEach((item) => {
+  item.addEventListener("click", toggleMenu);
+});
